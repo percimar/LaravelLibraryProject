02337e175a5cec8023edf36e27bbaef38d7ccd5c
Omar Ibrahim <60094882@cna-qatar.edu.qa>
2021-03-25 03:28:37 +0300

    Bookloader in laravel mix
    Added bookloader.js in the list of development files for webpack.
    Added bookloading along with csrf token.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,5 +85,6 @@
             @yield('content')
         </main>
     </div>
+    @yield('scripts')
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,10 @@
 <div class="grid-container">
   <!-- <div class="item1">Header</div> -->
   <div class="item2">Categories</div>
-  <div class="item3">Popular Books</div>  
+  <div class="item3">
+  <input id="searchBooks" placeholder="Search our books" />
+  <div id="loadBooks"><div>
+  </div>  
   <!-- <div class="item4">Right</div> -->
   <!-- <div class="item5">Footer</div> -->
 </div>
