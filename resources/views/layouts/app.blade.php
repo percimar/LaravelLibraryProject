8147ff12e78c505f6cbb3f97b8a82b3572baa6e1
Mahmoud <mahmou132@hotmail.com>
2021-03-25 10:44:15 +0300

    merge conflicts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,14 +71,11 @@
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-<<<<<<< Updated upstream
                                         {{ __('Logout') }}
                                     </a>
                                     <a class="dropdown-item" href="{{ route('contacts.create') }}">
                                         Contact
                                     </a>
-=======
-                                    {{ __('Logout') }}
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -86,7 +83,6 @@
                                 </form>
                             </div>
                         </li>
->>>>>>> Stashed changes
 
 
                         @endguest
