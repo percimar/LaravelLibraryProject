9afb9dd4eacb63ae032adfa1a397f67ce04b81e5
Omar Ibrahim <60094882@cna-qatar.edu.qa>
2021-03-28 21:53:49 +0300

    Dynamic book search completed

--- resources/js/loadBooks.js
+++ resources/js/loadBooks.js
@@ -1,16 +1,31 @@
 (async()=>{
     if (window.location.pathname === "/") {
-        let books = await $.get('/books',{
-            data: {
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        let searchField = document.getElementById('searchBooks')
+        let listener = searchField.addEventListener('keydown', send)
+
+        async function send(event) {
+
+            let books = await $.get('/books',{
+                data: {
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
                 }
-            }
-        })
-        
-        
-
-
+            })
+            
+            let loadDiv = $('#loadBooks')
+            loadDiv.text('')
+            books.map(book => {
+                if (book.title.indexOf(event.target.value) !== -1) {
+                    // Book CSS under append
+                    loadDiv.append(`
+                
+                    ${book.title}<br/>
+                    
+                    `)
+                }
+            })
+        }
     }
 })()
 
